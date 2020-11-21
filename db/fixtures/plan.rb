b7c32b75ef9fadfc2cb52f83b4b1a78b31a9f74f
Kazuya-Sakashita <kz0508@gmail.com>
2020-11-22 07:58:16 +0900

    予約システムオーター選択をDBから取得実装

---
+++ db/fixtures/plan.rb
@@ -0,0 +1,8 @@
+Plan.seed.once(:id,
+[
+{ id: 17, oreder: “テストカット17”, price: “1400”, description: “テストコメント17” },
+{ id: 18, oreder: “テストカット18”, price: “1300”, description: “テストコメント18” },
+{ id: 19, oreder: “テストカット19”, price: “1200”, description: “テストコメント19” },
+{ id: 20, oreder: “テストカット20”, price: “1100”, description: “テストコメント20” }
+]
+)
