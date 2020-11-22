ebff10e06b960bf684e70bcdc59aab83c11d3cdc
Kazuya-Sakashita <kz0508@gmail.com>
2020-11-23 05:41:46 +0900

    seed_fuを使用したデータ入力

--- db/fixtures/plan.rb
+++ db/fixtures/plan.rb
@@ -1,8 +1,6 @@
-Plan.seed.once(:id,
-[
-{ id: 17, oreder: “テストカット17”, price: “1400”, description: “テストコメント17” },
-{ id: 18, oreder: “テストカット18”, price: “1300”, description: “テストコメント18” },
-{ id: 19, oreder: “テストカット19”, price: “1200”, description: “テストコメント19” },
-{ id: 20, oreder: “テストカット20”, price: “1100”, description: “テストコメント20” }
-]
+Plan.seed_once(:id,
+{ id: "21", order: "test_cut17", price: "1400", description: "test_comment17" },
+{ id: "22", order: "test_cut18", price: "1300", description: "test_comment18" },
+{ id: "23", order: "test_cut19", price: "1200", description: "test_comment19" },
+{ id: "24", order: "test_cut20", price: "1100", description: "test_comment20" },
 )
