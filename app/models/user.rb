c97d9c9bd2ce3d5675566aa9d273b87666c64f04
Kazuya-Sakashita <kz0508@gmail.com>
2021-01-20 07:27:40 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
 
     validates_length_of :password, maximum: ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED, on: :update
     validates_confirmation_of :password, allow_blank: true, on: :update
-
+    validates :email, presence: true
+    validates :name, presence: true
+    validates :phone_number, presence: true
     has_many :reservations
     has_many :plans, through: :reservations
 
