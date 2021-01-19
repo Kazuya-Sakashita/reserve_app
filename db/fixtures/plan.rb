0d00d5f23e4107173bdbced65d21715825cda40b
Kazuya-Sakashita <kz0508@gmail.com>
2021-01-20 07:33:58 +0900

    commit

--- db/fixtures/plan.rb
+++
@@ -1,6 +0,0 @@
-Plan.seed_once(:id,
-{ id: "21", order: "test_cut17", price: "1400", description: "test_comment17" },
-{ id: "22", order: "test_cut18", price: "1300", description: "test_comment18" },
-{ id: "23", order: "test_cut19", price: "1200", description: "test_comment19" },
-{ id: "24", order: "test_cut20", price: "1100", description: "test_comment20" },
-)
