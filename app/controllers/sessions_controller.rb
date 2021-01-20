107bae0b746d64e505bf38e9fa8ccc89011b3383
Kazuya-Sakashita <kz0508@gmail.com>
2021-01-21 07:44:59 +0900

    ログインをメールだけに変更、空欄でログインをできなくする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,25 @@ class SessionsController < ApplicationController
   def create
     # メール & 電話での実装
     user = User.find_by(email: params[:email].downcase)
-    if user.nil?
-      user = User.find_by(phone_number: params[:phone_number].downcase)
-    end
+    # if user.nil?
+    #   user = User.find_by(phone_number: params[:phone_number].downcase)
+    # end
+    # if user.nil?
+    #   redirect_to login_path, alert: "you have to login" and return
+    # end
+
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to users_show_path(user.id)
+      redirect_to users_show_path(user.id) and return
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render 'new' and return
     end
   end
 
   def destroy
-    log_out
-    redirect_to root_path
+  session[:user_id] = nil
+  redirect_to root_path, notice: "you have successfully logout"
   end
 
 
