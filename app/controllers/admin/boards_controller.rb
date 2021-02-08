5a0f183eb9dff7e4fd1a8156a26c871561795c90
Kazuya-Sakashita <kz0508@gmail.com>
2021-02-08 20:02:52 +0900

    ○X表示の表を数値固定で実装

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -4,6 +4,31 @@ class Admin::BoardsController < ApplicationController
     @reservations = Reservation.all
   end
 
+  def show
+    @bloks = Block.all
+    @block_comment = Block.pluck(:block)
+    @day = "2021-02-11"
+    @day = @day.to_date
+    @reservation_day = Reservation.where(reservation_date:@day).pluck(:reservation_block).flatten!
+    @blocks_all = Block.pluck(:id)
+
+
+    @day_status = Array.new
+
+    @blocks_all.each do |block_status|
+      if @reservation_day.include?(block_status)
+          @day_status.push "X"
+      else
+          @day_status.push "○"
+      end
+    end
+    
+    @status = @block_comment.zip(@day_status).to_h
+  end
+
+
+
+
   private
     def admin_user
       redirect_to(root_path) unless current_user.admin?
