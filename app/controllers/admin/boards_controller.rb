eca90644c333d258328e9577850f2917d6a9d76c
Kazuya-Sakashita <kz0508@gmail.com>
2021-02-12 12:18:22 +0900

    SHOWの記述にミスがあり、修正

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -8,29 +8,28 @@ class Admin::BoardsController < ApplicationController
     @bloks = Block.all
     @block_comment = Block.pluck(:block)
     @staff_id = "1"
-    @day = "2021-02-11"
+    @day = "2021-02-10"
     @day = @day.to_date
-    
+    block_num2 = Block.pluck(:id).count
     @reservation_day = Reservation.where(staff_id:@staff_id).where(reservation_date:@day).pluck(:reservation_block).flatten
-    @blocks_all = Block.pluck(:id)
+    @block_id = Block.pluck(:id)
+    @day_status = Array.new
 
-
-    @day_status = Array.new　#表示のための○Xの配列を作成
-    if  @reservation_day.nil?#予約なしの場合
-      block_num2 = Block.pluck(:id).count#配列個数カウント
-      block_num2.times do |f| #配列個数分push繰返
+    if  
+      @reservation_day.nil?
+      block_num2.times do |f|
         @day_status.push "○"
         end
     elsif
         @block_id.each do |block_status|
-        if @reservation_day.include?(block_status)#予約ありはXを表示（判定）
-          @day_status.push "X"
-        else
-          @day_status.push "○"
-        end
+          if @reservation_day.include?(block_status)
+            @day_status.push "X"
+          else
+            @day_status.push "○"
+          end
       end
     end
-    @status = @block_comment.zip(@day_status).to_h #表示のために並べてハッシュ
+    @status = @block_comment.zip(@day_status).to_h
   end
 
 
