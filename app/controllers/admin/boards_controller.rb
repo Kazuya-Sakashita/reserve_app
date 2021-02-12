33714b60ee8532b49f99bc86c93fdd03fb6853ec
Kazuya-Sakashita <kz0508@gmail.com>
2021-02-12 20:26:24 +0900

    フォームで指定の日、指定のスタイリストから選択して表示する（スタイリスト名表示は未）

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -2,13 +2,15 @@ class Admin::BoardsController < ApplicationController
   before_action :admin_user
   def index
     @reservations = Reservation.all
+    @open_days = (Date.today..Date.today.advance(months:3)).select{|date| date.wday !=1}-Holiday.pluck(:closed_day)
+
   end
 
   def show
     @bloks = Block.all
     @block_comment = Block.pluck(:block)
-    @staff_id = "1"
-    @day = "2021-02-10"
+    @staff_id = params[:@staff_id]
+    @day =  params[:@day]
     @day = @day.to_date
     block_num2 = Block.pluck(:id).count
     @reservation_day = Reservation.where(staff_id:@staff_id).where(reservation_date:@day).pluck(:reservation_block).flatten
